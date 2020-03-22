77fcaea12cd86df33b1fec72cc9a1c2586500db2
Bartek Mis <bartek@studiosidekicks.com>
2020-03-22 23:59:43 +0100

    basic API req to get groups list

--- resources/js/views/admin-tools/groups/List.vue
+++ resources/js/views/admin-tools/groups/List.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    Groups list here
+    <v-progress-circular
+      v-if="isLoading"
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+
+    <div v-else>
+      List here.
+    </div>
   </div>
 </template>
+
+<script>
+import { getGroupsList } from '@/api/groups';
+
+export default {
+  name: 'cmsGroupsList',
+  data() {
+    return {
+      isLoading: false,
+      list: null
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.isLoading = true;
+      getGroupsList()
+        .then(response => {
+          console.log(response);
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    }
+  }
+};
+</script>
